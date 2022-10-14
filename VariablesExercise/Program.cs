1f9b708bf93301738465530f38de9d40a3e088e9
kyle jenne <strongagent1@gmail.com>
2022-10-14 12:49:39 -0400

    updated items again

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            string courseName = "TrueCoders"
+            string courseName = "TrueCoders";
 
-                int weeks = 9
+            int weeks = 9;
 
-                Console.WriteLine($"The best coding course is without question, I can't wait to begin coding in short weeks!");
+                Console.WriteLine($"The best coding course is {courseName} without question, I can't wait to begin coding in {weeks} short weeks!");
             
         }
     }
